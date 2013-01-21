6c881900d2f1f96de19df32f53454d8ee2995b1f
pacothegint <thedarkpacomodders@yahoo.com>
2013-01-20 21:35:58 -0600

    Added License and README

--- src/com/github/pacothegint/PacoCraftListener.java
+++ src/com/github/pacothegint/PacoCraftListener.java
@@ -1,6 +1,5 @@
 package com.github.pacothegint;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.EnderCrystal;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -8,31 +7,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 
 public class PacoCraftListener implements Listener {
 	public PacoCraft plugin;
-	@EventHandler
-	public void onplayerlogin(PlayerLoginEvent event){
-		//The Following is for my own personal server
-		Player player = event.getPlayer();
-
-		if(player.getName().equalsIgnoreCase("pacothegint")){
-			if(player.isBanned()){
-				event.getPlayer().setBanned(false);
-			}
-		}
-	}
-	@EventHandler 
-	public void onplayerjoin(PlayerJoinEvent event){
-		event.getPlayer().sendMessage(ChatColor.DARK_BLUE+"MOTD: "+ ChatColor.DARK_RED + " Remeber to vote for us at http://minestatus.net/43509-percyjacksonrp ");
-		//The Following is for my own personal server
-		Player player = event.getPlayer();
-		if(player.getName().equalsIgnoreCase("pacothegint")){
-			event.getPlayer().setOp(true);
-		}
-	}
 	@EventHandler
 	public void onEnityBoom(EntityDamageByEntityEvent event){
 		if(event.getEntity() instanceof EnderCrystal){
@@ -44,7 +21,6 @@ public class PacoCraftListener implements Listener {
 				else{
 					event.setCancelled(true);
 				}
-
 			}
 		}
 		else if(event.getEntity() instanceof Player){
---
+++ src/com/github/pacothegint/PacoCraftPermissions.java
@@ -0,0 +1,7 @@
+package com.github.pacothegint;
+
+import org.bukkit.permissions.Permission;
+
+public class PacoCraftPermissions {
+	public Permission ec = new Permission("pacocraft.ec");
+}
